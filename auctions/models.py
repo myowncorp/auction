0c1c2f99c0744fce287d35abf5396a2aa4d4183a
Juan <foruseasprogrammer@gmail.com>
2025-01-04 15:13:52 -0500

    added basic model

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,16 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    title = models.CharField(length=64)
+    description = models.CharField(length=500)
+    start_bid = models.IntegerField
+    image = models.URLField(blank=True)
+    category = models.CharField(length=50)
+    
+    
+class Bid(models.Model):
+    pass
+class Comments(models.Model):
+    pass
