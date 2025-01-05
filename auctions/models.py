f9acbf91e2a00521c43a18d01bfc27a74cf91c3c
Juan <foruseasprogrammer@gmail.com>
2025-01-04 21:04:38 -0500

    commit before merge

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+CATEGORY_CHOICES = (
+    ('Fashion', 'Fashion'),
+    ('BooksToys', "Books & Toys"),
+    ('Homegoods', 'Home Goods'),
+    ('Electronics', 'Electronics'))
 
 class User(AbstractUser):
     pass
@@ -10,10 +15,12 @@ class Listing(models.Model):
     description = models.CharField(max_length=500)
     start_bid = models.IntegerField(default=0)
     image = models.URLField(blank=True)
-    category = models.CharField(max_length=50)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
     
-    
+### TO DO
 class Bid(models.Model):
     pass
+
+### TO DO
 class Comments(models.Model):
     pass
